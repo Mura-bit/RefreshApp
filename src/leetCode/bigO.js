89a63b06ef152a3eff4c95d16c3dbac118ad28fc
Murat.bit <mura.usa11@gmail.com>
2024-04-29 22:13:55 -0700

    SecNum

--- src/leetCode/bigO.js
+++ src/leetCode/bigO.js
@@ -16,3 +16,30 @@ function pairSumSequence(n) {
 function pairSum(a, b) {
   return a + b;
 }
+
+function twoSum(nums, target) {
+  const numIndicates = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+    if (numIndicates.hasOwnProperty(complement)) {
+      return numIndicates[complement];
+    }
+    numIndicates[num] = i;
+  }
+  return [];
+}
+
+const nums1 = [2, 7, 11, 15];
+const target1 = 9;
+console.log(twoSum(nums1, target1));
+
+const nums2 = [3, 2, 4];
+const target2 = 6;
+console.log(twoSum(nums2, target2));
+
+const nums3 = [3, 3];
+const target3 = 6;
+console.log(twoSum(nums3, target3));
+
+export default twoSum;
